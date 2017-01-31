b8ed731d1d02735e1bf55f74f62a38930bc73a64
DarkCrucible <darkcrucible@sigaint.org>
2017-01-30 23:50:19 -0700

    add springboot starter

---
+++ src/main/java/com/darkcrucible/CosmicWindApplication.java
@@ -0,0 +1,12 @@
+package com.darkcrucible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicWindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmicWindApplication.class, args);
+	}
+}
